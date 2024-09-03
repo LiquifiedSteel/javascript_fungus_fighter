8a7e2e88c722b451f900fa6864927120dffd82a9
Alex Rivard <alex.rivard4406@gmail.com>
2024-09-02 19:07:50 -0500

    renderScreen and the arcane staff work as intended

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let fungalHealth = 100;
+let energy = 100;
+
 
 function onReady() {
     console.log("Ready to go!")
@@ -18,3 +21,40 @@ function onReady() {
 
 
 onReady()
+
+
+
+function renderScreen(){
+    const healthElement = document.getElementById("health");
+    const energyElement = document.getElementById("energy");
+    const fungusElement = document.getElementById("fungus");
+
+    if (fungalHealth < 0) {
+        fungalHealth = 0;
+    }
+    if (energy < 0) {
+        energy = 0;
+    }
+    if (fungalHealth === 0) {
+        fungusElement.classList.remove("walk");
+        fungusElement.classList.add("dead");
+    } else if (energy === 0) {
+        fungusElement.classList.remove("walk");
+        fungusElement.classList.add("jump");
+    }
+    healthElement.innerText = fungalHealth;
+    document.getElementById("hp-meter").value = fungalHealth;
+    energyElement.innerText = energy;
+    document.getElementById("ap-meter").value = energy;
+}
+
+function buttonClickArcane(event) {
+    if (energy < 18) {
+        event.target.classList.add("disabled")
+    } else {
+        fungalHealth -= 14;
+        energy -= 18;
+    }
+    renderScreen();
+}
+
